e82d42bccd7648c4ededc7b068a9b72df2b0d1c1
MBEY KOUMATEY <felicitembeykoumatey@gmail.com>
2021-12-04 18:23:12 +0100

    ajouter dotenv pour la variable d'environnement

--- back-end/models/sequelize.js
+++ back-end/models/sequelize.js
@@ -1,3 +1,49 @@
+require('dotenv').config()
 const Sequelize = require ('sequilize');
-const Sequelize = new Sequelize('sequelize-groupomania')
-;
+const Sequelize = new Sequelize (`${process.env.DB_BDD}`, `${process.env.DB_USER}`, `${process.env.DB_PASSWORD}`, {
+    dialect: "mysql",
+    host: "localhost"
+});
+sequelize.sync();
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.roles = require("./role.js")(sequelize, Sequelize);
+db.users = require("./user.js")(sequelize, Sequelize);
+db.posts = require("./post.js")(sequelize, Sequelize);
+db.comments = require("./Comment.js")(sequelize, Sequelize);
+
+//clés étrangères de Users
+
+db.roles.hasMany(db.users, { as: "users" });
+db.users.belongsTo(db.roles, {
+  foreignKey: "roleId",
+  as: "role",
+});
+
+//clés étrangères de Posts
+
+db.users.hasMany(db.posts, { as: "posts", onDelete: 'CASCADE' });
+db.posts.belongsTo(db.users, {
+  foreignKey: "userId",
+  as: "user",
+});
+
+//clés étrangères de Comments
+
+db.users.hasMany(db.comments, { as: "comments", onDelete: 'CASCADE' });
+db.comments.belongsTo(db.users, {
+  foreignKey: "userId",
+  as: "user",
+});
+
+db.posts.hasMany(db.comments, {as: "comments", onDelete: 'CASCADE' });
+db.comments.belongsTo(db.posts, {
+  foreignKey: "postId",
+  as: "post",
+});
+
+module.exports = db;
