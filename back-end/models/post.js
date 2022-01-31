93341fbd7316ed8ae6c24912613d1ff4c93cbc5f
MBEY KOUMATEY <felicitembeykoumatey@gmail.com>
2022-01-31 20:06:54 +0100

    création model post

---
+++ back-end/models/post.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, Sequelize) => {
+  const Post = sequelize.define('post', {
+    content: {
+      type: Sequelize.TEXT,
+      allowNull: false
+    },
+    image: {
+      type: Sequelize.STRING(191)
+    },
+    date: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW
+    }
+  }, {
+    timestamps: false
+  });
+
+  return Post;
+};
