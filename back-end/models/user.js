93f918a642b01b9fd4cbcf24c6e3973fb1662606
MBEY KOUMATEY <felicitembeykoumatey@gmail.com>
2022-02-03 11:38:23 +0100

    modification de notre modèle  table user

--- back-end/models/user.js
+++ back-end/models/user.js
@@ -12,17 +12,30 @@ const User = sequelize.define('user', {
   // Column-1, user_id est un objet avec
   // propriétés comme le type, les clés,
   // validation de la colonne.
-  user_id:{
-    type:Sequelize.INTEGER,// Le module Sequelize a INTEGER Data_Type.
-    autoIncrement:true, // To increment user_id automatically.
-    allowNull:false,// user_id ne peut pas être nul.
-    primaryKey:true// Pour identifier l'utilisateur de manière unique.
+  //user_id:{
+  //type:Sequelize.INTEGER,// Le module Sequelize a INTEGER Data_Type.
+  //autoIncrement:true, // Pour incrémenter user_id automatiquement.
+  // allowNull:false,// user_id ne peut pas être nul.
+  // primaryKey:true// Pour identifier l'utilisateur de manière unique.
+  // },
+  username: {
+    type: Sequelize.STRING(20),
+    allowNull: false
   },
-  name: { type: Sequelize.STRING, allowNull:false },  // Column-2, name
-  email: { type: Sequelize.STRING, allowNull:false }, // Column-3, email
-  
-  // Colonne-4, valeurs par défaut
-  // dates => heure actuelle
+  email: {
+    type: Sequelize.STRING(191),
+    allowNull: false,
+    unique : 'uniqueIndex'
+  },
+  password: {
+    type: Sequelize.STRING(100),
+    allowNull: false
+  },
+ 
+  sex: {
+    type: Sequelize.STRING(1),
+    allowNull: false
+  },
   myDate: { type: Sequelize.DATE,
     defaultValue: Sequelize.NOW },
   
