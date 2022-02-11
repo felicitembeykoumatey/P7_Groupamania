3022316245c4e5fc8ca0d37a41cd80ccbeb6c770
MBEY KOUMATEY <felicitembeykoumatey@gmail.com>
2022-02-12 00:15:45 +0100

    Indenter le code et rajouter des commentaires

--- back-end/routes/post.js
+++ back-end/routes/post.js
@@ -4,29 +4,21 @@ const express = require("express"); // chargé librairie express
 const router = express.Router(); // router 
 const auth = require('../middleware/auth'); // authentification
 const multer = require("../middleware/multer-config"); // chargé package multer  pour la  gestion des fichiers ( images des différents formats)
-
-//Chargé le fichier controllers 
-const postCtrollers = require('../controllers/postCtlr');
+const postCtrollers = require('../controllers/postCtlr');//Chargé le fichier controllers 
 
 // Création des routes  GET POST PUT DELETE
 
 //Création d'un post
 router.post('/', auth, multer, postCtrollers.createPost);
-
 //Récupérer Tous les posts 
 router.get('/', auth, postCtrollers.getAllPost);
-
 //Récupérer un post par id d'un utilisateur
 router.get('/:id', auth, postCtrollers.getOnePost);
-
 //Récupérer Tous les posts d'un utilisateur
 router.get('/user/:userId', auth, postCtrollers.getAllPostFromOneUser);
-
 //Modifier post
 router.put('/:id', auth, multer, postCtrollers.modifyPost);
-
 //Supprimer post
 router.delete('/:id', auth, postCtrollers.deletePost);
-
 
 module.exports = router;
