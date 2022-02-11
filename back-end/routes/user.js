7b79c5d42b532a9d74cd6ba12c65657bc540872f
MBEY KOUMATEY <felicitembeykoumatey@gmail.com>
2022-02-12 00:18:26 +0100

    suppression lignes inutiles et poster une image depuis postman

--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -6,19 +6,16 @@ const router = express.Router();
 //const auth = require ('../middleware/auth');
 
 const limiter = require ('../middleware/limiter');// middeleware limiter //
-// controllers user//
+//Chargé le fichier controllers 
 const userCtrl = require ('../controllers/userCtlr');// Réécupérer le controleur pour "user"
-console.log("userCtrl");
+
 
 /**ROUTES**/
 //Création des routes  inscription et connexion //
 
 router.post('/signup', limiter, userCtrl.signup);// Inscription//
 router.post('/login', limiter, userCtrl.login); // Connexion//
-//router.delete('/:id', auth, userCtrl.deleteAccount);
-//router.get('/:id', auth, userCtrl.getOneAccount);
-//router.put('/:id', auth, userCtrl.modifyAccount);
-//router.get('/accounts', userCtrl.getAllAccounts);
+
 
 //**EXPORT**/
 module.exports = router;
