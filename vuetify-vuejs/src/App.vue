b7672d9d1cd67aa07fdd22c0d35e812e6e026eef
MBEY KOUMATEY <felicitembeykoumatey@gmail.com>
2022-03-02 18:43:37 +0100

    création fichier vue

---
+++ vuetify-vuejs/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-main>
+        <headerTop></headerTop>
+         <cartePost></cartePost>
+        <footerBottom></footerBottom>
+      </v-main>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import headerTop from './components/headerTop.vue'
+import footerBottom from './components/footerBottom.vue'
+import cartePost from './components/cartePost.vue'
+export default{
+  name:'app',
+  components: {
+    'headerTop' : headerTop,
+    'footerBottom': footerBottom,
+    'cartePost':cartePost
+  },
+}
+
+</script>
+
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
+<!-- <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+  </div> -->
---
+++ vuetify-vuejs/src/components/headerTop.vue
@@ -0,0 +1,11 @@
+<template>
+ <h1>Groupomania, réseau social d'entreprise</h1>
+
+</template>
+
+<script>
+  export default {
+    name: "headerTop",
+  };
+</script>
+
