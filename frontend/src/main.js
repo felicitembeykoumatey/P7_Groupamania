2cdaaf8b916af9e833d23f6c7b44a5dde4f3e781
MBEY KOUMATEY <felicitembeykoumatey@gmail.com>
2022-03-21 02:03:15 +0100

    recherche solution pour afficher le contenu de mon site

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,11 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
 import axios from 'axios'
-import VueAxios from 'vue-axios'
-const app=createApp(App);
 
 Vue.config.productionTip = false
-app.use(router);
-app.use(VueAxios, axios) 
-app.mount('#app')
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -7,7 +7,9 @@
 Connectez-vous et restez en contact avec vos collègues.
 Ensemble amusons nous!
 </p>
-
+  <router-link class="redirection-login" to="/login"><button v-on:click="userLogin" type="submit" class="btn-connexion" value="Connecté">Se connecter</button></router-link>
+  <br>
+   <router-link class="redirection-signup" to="/signup"><button v-on:click="userLogin" type="submit" class="btn-inscription" value="Connecté">S'inscrire</button></router-link>
 </section>
 </main>
 
