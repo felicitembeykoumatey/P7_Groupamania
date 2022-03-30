505211977e2c2330a881f1806207a118c8045640
MBEY KOUMATEY <felicitembeykoumatey@gmail.com>
2022-03-30 15:22:17 +0200

    fermeture crochet []

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,71 +1,55 @@
-import { createRouter, createWebHistory} from 'vue-router';
+import {
+  createRouter,
+  createWebHistory
+} from 'vue-router'
 
-import Signup from './pages/Signup.vue';
-import Login from './pages/Login.vue';
-import Home from './pages/Home.vue';
-import EditPost from './pages/EditPost.vue';
-import UserProfile from './pages/UserProfile.vue';
-<<<<<<< HEAD
+import Signup from './pages/Signup.vue'
+import Login from './pages/Login.vue'
+import Home from './pages/Home.vue'
+import EditPost from './pages/EditPost.vue'
+import UserProfile from './pages/UserProfile.vue'
 
-=======
-import Post from './pages/Post.vue';
-import AllProfiles from './pages/AllProfiles.vue';
->>>>>>> main
+const routes = [
 
-const routes =[
+  // Chemin vers le portail pour s'inscrire ou se connecter, avant d'entrer dans Home
+  {
+    name: 'Home',
+    component: Home,
+    path: '/'
+  },
 
-     // Chemin vers le portail pour s'inscrire ou se connecter, avant d'entrer dans Home
-   {
-         name:'Home',
-         component : Home,
-        path:'/'
-    }, 
+  // Chemin vers la page connexion
+  {
+    name: 'Login',
+    component: Login,
+    path: '/login'
+  },
+  // Chemin vers la page d'inscription
+  {
+    name: 'Signup',
+    component: Signup,
+    path: '/signup'
+  },
 
-    // Chemin vers la page connexion
-    {
-         name:'Login',
-         component : Login,
-        path:'/login'
-    },
-      // Chemin vers la page d'inscription
-      {
-         name:'Signup',
-         component : Signup,
-        path:'/signup'
-    }, 
-   
-    // Chemin vers la page actualités(journal)
-      {
-         name:'EditPost',
-         component : EditPost,
-        path:'/posts'
-    }, 
-   
-  
-    
-    // Chemin vers la page profile utilisateur
-      {
-         name:'UserProfile',
-         component : UserProfile,
-        path:'/profil'
-    }, 
+  // Chemin vers la page actualités(journal)
+  {
+    name: 'EditPost',
+    component: EditPost,
+    path: '/posts'
+  },
 
-<<<<<<< HEAD
-   
-=======
-    // Chemin vers tous les profiles utilisateurs
-      {
-         name:'AllProfiles',
-         component : AllProfile,
-        path:'/allprofiles'
-    }, 
->>>>>>> main
-];
-const router = createRouter ({
+  // Chemin vers la page profile utilisateur
+  {
+    name: 'UserProfile',
+    component: UserProfile,
+    path: '/profil'
+  }
+]
 
-    history: createWebHistory(),
-    routes,
+const router = createRouter({
+
+  history: createWebHistory(),
+  routes
 })
 
-
-export default router;
+export default router
