b00ad45ae6c68e45db74db78bf657d8cb6fc88a7
MBEY KOUMATEY <felicitembeykoumatey@gmail.com>
2022-03-30 15:21:44 +0200

    suppression message erreur
    proposition solution par vscode <<< head

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -5,26 +5,12 @@
         <p>Vous pouvez désormais discuter et apprendre à connaitre vos collègues</p>
     <span class="link-signup"><router-link class="redirection-signup" to="/signup"> <button v-on:click="userSignup" type="submit" class="btn-signup" value="s'inscrire">S'inscrire</button> </router-link></span>
 
-<<<<<<< HEAD
     <span class="link-login"><router-link class="redirection-login" to="/login"> <button v-on:click="userLogin" type="submit" class="btn-login" value="Connecté">Se connecter</button> </router-link>
     </span>
     </section>
     </main>
 </template>
 
-=======
-<section class="welcome">
-<h1>Bienvenu(e) au reseau social d'entreprise</h1>
-<p class="msg-accueil">
-Connectez-vous et restez en contact avec vos collègues.
-Ensemble amusons nous!
-</p>
-  <router-link class="redirection-login" to="/login"><button v-on:click="userLogin" type="submit" class="btn-connexion" value="Connecté">Se connecter</button></router-link>
-  <br>
-   <router-link class="redirection-signup" to="/signup"><button v-on:click="userLogin" type="submit" class="btn-inscription" value="Connecté">S'inscrire</button></router-link>
-</section>
-</main>
->>>>>>> main
 
 <script>
 export default {
