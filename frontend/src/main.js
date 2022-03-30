fa9d1331503815416f98d7f03a9e977eb9b13b8f
MBEY KOUMATEY <felicitembeykoumatey@gmail.com>
2022-03-30 15:20:56 +0200

    correction liée aux dependances

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,29 +1,10 @@
-HEAD
-import { createApp } from "vue";
-import App from './App.vue';
+import {
+  createApp
+} from 'vue'
+import App from './App.vue'
 import router from './router'
-window.axios = require('axios');
+window.axios = require('axios')
+
 import store from './store'
-//import axios from 'axios'
-   // Vue.use(axios)
 
-
-
-createApp(App)
-.use(store)
-    .use(router)
-
-    .mount("#app");
-
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import axios from 'axios';
-
-Vue.config.productionTip = false;
-
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app');
-
+createApp(App).use(store).use(router).mount('#app')
